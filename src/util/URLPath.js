b5edb23697b98ebe3d7484dc1cddda5b1bacc92e
mjpgang <oozsub8@gmail.com>
2026-09-11 21:45:55 +0900

    Update URLPath.js

--- src/util/URLPath.js
+++ src/util/URLPath.js
@@ -1,20 +1,50 @@
 /**
- * for lazy people who don't want to type out `new URL('http://blah' + req.url).searchParams.get(ugh)` all the time
+ * URL helper for request paths.
  */
+
 module.exports = class URLPath extends URL {
     /**
-     * @param {string} path - /site/path
+     * @param {string} path
      */
     constructor(path) {
-        super(path, 'http://foobar');
+        let value = String(path || '/');
+
+        // Node/Vercel may pass an invalid proxy-style path.
+        // URL requires a valid absolute or relative URL.
+        if (!value.startsWith('/') && !value.startsWith('http://') && !value.startsWith('https://')) {
+            value = '/' + value;
+        }
+
+        // Some Rammerhead proxy paths contain a colon in the path.
+        // Encode only the problematic colon when it is not a scheme.
+        if (
+            value.startsWith('/') &&
+            value.includes(':') &&
+            !value.startsWith('/http://') &&
+            !value.startsWith('/https://')
+        ) {
+            const firstColon = value.indexOf(':');
+            const firstSlash = value.indexOf('/');
+
+            if (firstColon > firstSlash) {
+                value =
+                    value.slice(0, firstColon) +
+                    '%3A' +
+                    value.slice(firstColon + 1);
+            }
+        }
+
+        super(value, 'http://foobar');
     }
+
     /**
-     * @param {string} param - ?param=value
+     * @param {string} param
      * @returns {string|null}
      */
     get(param) {
         return this.searchParams.get(param);
     }
+
     /**
      * @returns {{[param: string]: string}}
      */
